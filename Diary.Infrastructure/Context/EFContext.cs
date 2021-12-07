b9ad9e4503767a98c135726ffb5af1856f6e3b7c
assis-silva <assislimacontato@gmail.com>
2021-12-07 15:29:38 -0300

    Setting Mgraton environment

--- Diary.Infrastructure/Context/EFContext.cs
+++ Diary.Infrastructure/Context/EFContext.cs
@@ -1,16 +1,24 @@
-﻿using Diary.Application.Interfaces;
-using Diary.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
 using System.Threading.Tasks;
+using Diary.Application.Interfaces;
+using Diary.Domain.Entities;
 
 namespace Diary.Infrastructure.Context
 {
     public class EFContext : DbContext, IEFContext
     {
+        public EFContext(DbContextOptions<EFContext> options) : base(options) { }
         public DbSet<DiaryPage> Diario { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(EFContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
---
+++ Diary.Infrastructure/IoC/InfraExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Diary.Application.Interfaces;
+using Diary.Infrastructure.Context;
+
+
+namespace Diary.Infrastructure.IoC
+{
+    public static class InfraExtensions
+    {
+        public static IServiceCollection InfraRegister(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EFContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("SqlConnection"));
+            });
+
+            services.AddTransient<IEFContext, EFContext>();
+            return services;
+        }
+    }
+}
